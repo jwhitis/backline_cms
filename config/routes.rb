bb962b0e91fcdd23245e1ee497444b30387aeb62
jwhitis <jack.whitis@gmail.com>
2014-02-04 22:44:50 -0600

    Make releases the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Toddfarrell::Application.routes.draw do
   get "releases", to: "pages#releases"
   get "media", to: "pages#media"
   get "bio", to: "pages#bio"
-  root "pages#shows"
+  root "pages#releases"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
