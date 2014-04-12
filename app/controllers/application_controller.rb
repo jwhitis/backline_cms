2c058a5f7f086c4e638c72e6d0af319c18fe1905
jwhitis <jack.whitis@gmail.com>
2014-04-11 22:43:06 -0500

    Update Twitter gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,24 @@ class ApplicationController < ActionController::Base
 
   TWITTER_HANDLES = ["ToddFarrellJr", "SomeCallMeGoose", "EliRhodes", "jwhitis"]
 
+  protected
+
   def expose_tweets
-    unless request.xhr?
-      @tweets = []
-      TWITTER_HANDLES.each { |handle| @tweets.concat Twitter.user_timeline(handle) }
-      @tweets = @tweets.sort_by(&:created_at).reverse!.take(7)
+    @tweets = sorted_tweets.take(7) unless request.xhr?
+  end
+
+  def sorted_tweets
+    tweets = []
+    TWITTER_HANDLES.each { |handle| tweets.concat twitter_client.user_timeline(handle) }
+    tweets.sort_by(&:created_at).reverse
+  end
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
   end
 
