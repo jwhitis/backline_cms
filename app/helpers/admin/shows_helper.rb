3dea0ed37c15dbdf6d34df3ebd34cac1ca2f72e3
jwhitis <jack.whitis@gmail.com>
2014-04-14 23:19:20 -0500

    Show titles are formatted automatically

--- app/helpers/admin/shows_helper.rb
+++ app/helpers/admin/shows_helper.rb
@@ -1,2 +1,11 @@
 module Admin::ShowsHelper
+
+  def show_title show
+    title = "#{show.city}"
+    title += ", " if show.city.present? && show.state.present?
+    title += "#{show.state}"
+    title += " - " if (show.city.present? || show.state.present?) && show.venue.present?
+    title += "#{show.venue}"
+  end
+
 end
