7d803c89812465f6e7129fc674131b02550f4cc3
jwhitis <jack.whitis@gmail.com>
2015-08-02 10:37:25 -0500

    Use nested layout for admin content

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AdminController < ApplicationController
   before_action :authenticate_admin!
+  layout "admin"
 
   def index
     @pages = Page.editable.display_order.page(params[:page_number])
