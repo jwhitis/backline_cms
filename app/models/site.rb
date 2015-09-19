4e62d0775f106432d2075fd38fc049f6b9c546b3
jwhitis <jack.whitis@gmail.com>
2015-09-19 09:43:03 -0500

    Sites validate that home page is published

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,12 @@
 class Site < ActiveRecord::Base
-  validates_presence_of :homepage_id
+  validates_presence_of :title, :homepage_id
+  validate :homepage_must_be_published
+
+  def homepage_must_be_published
+    unless Page.published.find_by(id: self.homepage_id)
+      errors.add(:homepage_id, "must be a published page")
+    end
+  end
 
   def reload options = nil
     @homepage = nil
