2bf0e4f93b78cd735431ad349bdb54361bf8ddfc
jwhitis <jack.whitis@gmail.com>
2015-10-27 07:45:06 -0500

    Add Theme class

---
+++ app/domain/theme.rb
@@ -0,0 +1,30 @@
+class Theme
+  class << self
+
+    def defaults
+      @defaults ||= begin
+        theme = hashes.first
+        theme.last.symbolize_keys.merge(title: theme.first)
+      end
+    end
+
+    def hashes
+      @hashes ||= begin
+        raw_themes = File.read(file_path)
+        YAML.load(raw_themes)
+      end
+    end
+
+    def reload
+      @defaults = nil
+      @hashes   = nil
+    end
+
+    private
+
+    def file_path
+      File.join(Rails.root, "lib", "assets", "themes.yml")
+    end
+
+  end
+end
