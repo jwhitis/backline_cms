3c4b351ae5846755c8c65a3bbb99b9139f40b7c5
jwhitis <jack.whitis@gmail.com>
2015-12-06 18:08:44 -0600

    Move logic for compiling site CSS into SiteCSSCompiler class

---
+++ app/domain/site_css_compiler.rb
@@ -0,0 +1,39 @@
+class SiteCSSCompiler
+
+  def initialize site
+    @site = site
+  end
+
+  def css
+    [
+      background_image_css,
+      banner_image_css,
+      display_font_css,
+      body_font_css,
+      @site.css
+    ].reject(&:blank?).join("\n")
+  end
+
+  private
+
+  def background_image_css
+    return "" if @site.background_image_url.nil?
+
+    "div#main { background-image: url(#{@site.background_image_url}); }"
+  end
+
+  def banner_image_css
+    return "" if @site.banner_image_url.nil?
+
+    "div#banner { background-image: url(#{@site.banner_image_url}); }"
+  end
+
+  def display_font_css
+    ".display-font, .page-title, .btn-main, .flash-main { font-family: #{@site.display_font}; }"
+  end
+
+  def body_font_css
+    "body, h1, h2, h3, h4, h5, h6 { font-family: #{@site.body_font}; }"
+  end
+
+end
--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,31 +1,10 @@
 module SitesHelper
 
   def site_style_tag
-    css = [background_image_css, banner_image_css, display_font_css, body_font_css, @site.css].
-      join("\n")
+    css = SiteCSSCompiler.new(@site).css
     return "" if css.blank?
 
     content_tag(:style, css.html_safe, id: "site-css")
-  end
-
-  def background_image_css
-    return "" if @site.background_image_url.nil?
-
-    "div#main { background-image: url(#{@site.background_image_url}); }"
-  end
-
-  def banner_image_css
-    return "" if @site.banner_image_url.nil?
-
-    "div#banner { background-image: url(#{@site.banner_image_url}); }"
-  end
-
-  def display_font_css
-    ".display-font, .page-title, .btn-main, .flash-main { font-family: #{@site.display_font}; }"
-  end
-
-  def body_font_css
-    "body, h1, h2, h3, h4, h5, h6 { font-family: #{@site.body_font}; }"
   end
 
 end
