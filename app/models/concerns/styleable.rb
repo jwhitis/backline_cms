83ed7c73b362f56d71d65be351fb6204a5698846
jwhitis <jack.whitis@gmail.com>
2015-12-06 18:01:00 -0600

    Move conditional statement out of validation method and onto #validate macro

--- app/models/concerns/styleable.rb
+++ app/models/concerns/styleable.rb
@@ -2,10 +2,10 @@ module Styleable
   extend ActiveSupport::Concern
 
   included do
-    validate :scss_compiles_to_css
+    validate :scss_compiles_to_css, if: :scss_changed?
 
     def scss_compiles_to_css
-      self.css = Sass.compile(scoped_scss) if self.scss_changed?
+      self.css = Sass.compile(scoped_scss)
     rescue Sass::SyntaxError => exception
       self.errors[:base] << exception.message
     end
