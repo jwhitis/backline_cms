6bcc08229294eb32deda196a7206c4035f6db626
jwhitis <jack.whitis@gmail.com>
2016-01-06 08:12:55 -0600

    Refactor class method for finding inactive features; update seeds file to work with site validations

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -5,9 +5,10 @@ class Feature < ActiveRecord::Base
 
   validates :name, presence: true, inclusion: { in: NAMES }, uniqueness: { case_sensitive: false }
 
-  def self.unactivated
+  def self.inactive
     joins("LEFT OUTER JOIN feature_activations ON feature_activations.feature_id = features.id").
-      where(feature_activations: { feature_id: nil })
+      where("feature_activations.site_id IS NULL OR feature_activations.site_id != ?", Backline.site.id).
+      group("features.id")
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,16 @@ Feature::NAMES.each do |name|
   Feature.find_or_create_by!(name: name)
 end
 
+site = Site.create_with(title: "New Site").first_or_create!
+site.features << Feature.inactive
+
 DefaultPage::SLUGS.each do |slug|
   page = DefaultPageCreator.new(slug).first_or_create!
   NavLinkCreator.new(page).first_or_create!
 end
 
 home_page = Page.published.order(:created_at).first
-site = Site.create_with(title: "New Site", home_page_id: home_page.id).first_or_create!
-site.features << Feature.unactivated
+site.update_attributes!(home_page_id: home_page.id)
 
 design = site.design || site.create_design!
 design.create_color_scheme! unless design.color_scheme
@@ -27,4 +29,4 @@ user = User.create_with(
   password: "password",
   password_confirmation: "password"
 ).first_or_create!
-user.roles.create!(name: "site_admin", site: site)
+user.roles.create_with(name: "site_admin").find_or_create_by!(site: site)
