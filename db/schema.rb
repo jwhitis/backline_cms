3f081b7412a30ff04935513f6ad86f4332194c11
jwhitis <jack.whitis@gmail.com>
2016-01-09 07:34:39 -0600

    Add perishable_token column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104142236) do
+ActiveRecord::Schema.define(version: 20160108135359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -219,6 +219,7 @@ ActiveRecord::Schema.define(version: 20160104142236) do
     t.string   "last_login_ip"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "perishable_token",               null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
