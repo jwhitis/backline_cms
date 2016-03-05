3ee5ef3bef0a7ccf1b2a40c6919c76ff140cb2c0
jwhitis <jack.whitis@gmail.com>
2016-03-05 15:36:25 -0600

    Configure Action Mailer to use SendGrid in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
 
   # Set the default options for URLs generated in emails.
   config.action_mailer.default_url_options = { host: "backlinecms.com" }
+
+  # Configure Action Mailer to use SendGrid.
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    enable_starttls_auto: true
+  }
 end
