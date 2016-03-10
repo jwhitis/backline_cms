cf8314bd0794d309c09e3e9353a11545c7b9671c
jwhitis <jack.whitis@gmail.com>
2016-03-10 08:23:22 -0600

    Add super_admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309233108) do
+ActiveRecord::Schema.define(version: 20160310132910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,20 +231,21 @@ ActiveRecord::Schema.define(version: 20160309233108) do
   add_index "twitter_handles", ["site_id"], name: "index_twitter_handles_on_site_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                          null: false
-    t.string   "crypted_password",               null: false
-    t.string   "password_salt",                  null: false
-    t.string   "persistence_token",              null: false
-    t.integer  "login_count",        default: 0, null: false
-    t.integer  "failed_login_count", default: 0, null: false
+    t.string   "email",                              null: false
+    t.string   "crypted_password",                   null: false
+    t.string   "password_salt",                      null: false
+    t.string   "persistence_token",                  null: false
+    t.integer  "login_count",        default: 0,     null: false
+    t.integer  "failed_login_count", default: 0,     null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
     t.string   "current_login_ip"
     t.string   "last_login_ip"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "perishable_token",               null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "perishable_token",                   null: false
+    t.boolean  "super_admin",        default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
