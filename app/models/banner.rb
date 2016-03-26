877f8ec5af493ad30f0f323e15e1b452425f60b4
jwhitis <jack.whitis@gmail.com>
2016-03-26 10:25:31 -0500

    Automatically destroy blank banner links

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -4,7 +4,15 @@ class Banner < ActiveRecord::Base
   belongs_to :design
   has_one :link, class_name: "BannerLink", inverse_of: :banner, dependent: :destroy
 
+  accepts_nested_attributes_for :link, reject_if: :reject_link, allow_destroy: true
+
   validates_presence_of :image, :design_id
 
-  accepts_nested_attributes_for :link, reject_if: :all_blank
+  def reject_link attributes
+    persisted = attributes[:id].present?
+    blank = attributes.except(:id).values.all?(&:blank?)
+    attributes.merge!(_destroy: true) if persisted && blank
+    !persisted && blank
+  end
+
 end
